c999697cd8536c561cba71d6227fa42ecb622c02
vhumenny <v.humenny@gmail.com>
2022-10-27 21:21:03 +0300

    file reader and file writer

---
+++ src/work_with_files/fileReaderEx.java
@@ -0,0 +1,20 @@
+package work_with_files;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class fileReaderEx {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("test2.txt")) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                System.out.print((char)character);
+            }
+            System.out.println();
+            System.out.println("Done.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/work_with_files/fileWriterEx.java
@@ -0,0 +1,25 @@
+package work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class fileWriterEx {
+    public static void main(String[] args) {
+        String rubai= "Много лет я размышлял над жизнью земной.\n" +
+                "Непонятного нет у меня под луной.\n" +
+                "Мне известно, что мне ничего не известно!\n" +
+                "Вот последняя правда, открытая мной.\n";
+        String s = "privet!";
+
+        try (FileWriter writer= new FileWriter("test2.txt", true)){
+            for (int i = 0; i < rubai.length(); i++) {
+                writer.write(rubai.charAt(i));
+            }
+            writer.write(rubai);
+            writer.write(s);
+            System.out.println("Done!");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
