df07ef0fc97b70e0ab645ef0ddea6b086d0b5881
vhumenny <v.humenny@gmail.com>
2022-10-31 22:04:12 +0200

    threads

---
+++ src/multithreading/Ex5.java
@@ -0,0 +1,17 @@
+package multithreading;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        MyThread5 myThread5 = new MyThread5();
+        myThread5.setName("my thread");
+        myThread5.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Name of myThread5 = " + myThread5.getName() +
+                " Priority of myThread5 = " + myThread5.getPriority());
+    }
+}
+
+class MyThread5 extends Thread {
+    public void run() {
+        System.out.println("privet");
+    }
+}
