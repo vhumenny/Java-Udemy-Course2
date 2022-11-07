f155039563c25edba6385622bc4f6ba670f7328c
vhumenny <v.humenny@gmail.com>
2022-11-07 11:45:14 +0200

    linkedList, listIterator and BinarySearch

--- src/collections/BinarySearch.java
+++ src/collections/BinarySearch.java
@@ -1,49 +1,69 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class BinarySearch {
     public static void main(String[] args) {
-//        ArrayList<Integer> arrayList = new ArrayList<>();
-//        arrayList.add(-3);
-//        arrayList.add(8);
-//        arrayList.add(12);
-//        arrayList.add(-8);
-//        arrayList.add(0);
-//        arrayList.add(5);
-//        arrayList.add(10);
-//        arrayList.add(1);
-//        arrayList.add(150);
-//        arrayList.add(-30);
-//        arrayList.add(19);
-//        Collections.sort(arrayList);
-//        System.out.println(arrayList);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(-3);
+        arrayList.add(8);
+        arrayList.add(12);
+        arrayList.add(-8);
+        arrayList.add(0);
+        arrayList.add(5);
+        arrayList.add(10);
+        arrayList.add(1);
+        arrayList.add(150);
+        arrayList.add(-30);
+        arrayList.add(19);
+        Collections.sort(arrayList);
+        System.out.println(arrayList);
+        Collections.reverse(arrayList);
+        System.out.println(arrayList);
+        Collections.shuffle(arrayList);
+        System.out.println(arrayList);
+
+
 //        int index1 = Collections.binarySearch(arrayList, 12);
 //        System.out.println(index1);
 
-        Employee emp1 = new Employee(100,"Vova", 12345);
-        Employee emp2 = new Employee(15,"Anna", 6542);
-        Employee emp3 = new Employee(123,"Artem", 8542);
-        Employee emp4 = new Employee(15,"Artur", 5678);
-        Employee emp5 = new Employee(182,"Vasya", 125);
-        Employee emp6 = new Employee(15,"Alla", 9874);
-        Employee emp7 = new Employee(250,"Boris", 1579);
-        List <Employee> employeeList = new ArrayList<>();
-        employeeList.add(emp1);
-        employeeList.add(emp2);
-        employeeList.add(emp3);
-        employeeList.add(emp4);
-        employeeList.add(emp5);
-        employeeList.add(emp6);
-        employeeList.add(emp7);
-        System.out.println(employeeList);
-        Collections.sort(employeeList);
+//        Employee emp1 = new Employee(100, "Vova", 12345);
+//        Employee emp2 = new Employee(15, "Anna", 6542);
+//        Employee emp3 = new Employee(123, "Artem", 8542);
+//        Employee emp4 = new Employee(15, "Artur", 5678);
+//        Employee emp5 = new Employee(182, "Vasya", 125);
+//        Employee emp6 = new Employee(15, "Alla", 9874);
+//        Employee emp7 = new Employee(250, "Boris", 1579);
+//        List<Employee> employeeList = new ArrayList<>();
+//        employeeList.add(emp1);
+//        employeeList.add(emp2);
+//        employeeList.add(emp3);
+//        employeeList.add(emp4);
+//        employeeList.add(emp5);
+//        employeeList.add(emp6);
+//        employeeList.add(emp7);
+//        System.out.println(employeeList);
+//        Collections.sort(employeeList);
+//        System.out.println(employeeList);
+//
+//        int index2 = Collections.binarySearch(employeeList,
+//                new Employee(123, "Artem", 8542));
+//        System.out.println(index2);
+
+//        int[] array = {-3,8,12,-8,0,5,10,1,150,-30,19};
+//        System.out.println(Arrays.toString(array));
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+//        int index3 = Arrays.binarySearch(array, 150);
+//        System.out.println(index3);
+
     }
 }
 
-class Employee implements Comparable {
+class Employee implements Comparable<Employee> {
     int id;
     String name;
     int salary;
@@ -64,7 +84,11 @@ class Employee implements Comparable {
     }
 
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(Employee anotherEmp) {
+        int result = this.id - anotherEmp.id;
+        if (result == 0) {
+            result = this.name.compareTo(anotherEmp.name);
+        }
+        return result;
     }
 }
