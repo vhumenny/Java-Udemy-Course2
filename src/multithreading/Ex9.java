e8dee422691029c95c775a395140e5819c514a48
vhumenny <v.humenny@gmail.com>
2023-05-18 18:30:53 +0300

    RandomAccessFile

--- src/multithreading/Ex9.java
+++ src/multithreading/Ex9.java
@@ -1,28 +1,48 @@
 package multithreading;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
 public class Ex9 {
-    public static void main(String[] args) throws InterruptedException {
+    //    public static void main(String[] args) throws InterruptedException {
+//
+////        System.out.println("Method main begins");
+////        Thread thread = new Thread(new Worker());
+////
+////        System.out.println(thread.getState());
+////        thread.start();
+////        System.out.println(thread.getState());
+////        thread.join();
+////        System.out.println(thread.getState());
+////
+////        System.out.println("Method main ends");
+//
+//        method();
+//    }
+    int a = 10;
 
-        System.out.println("Method main begins");
-        Thread thread = new Thread(new Worker());
-        System.out.println(thread.getState());
-        thread.start();
-        System.out.println(thread.getState());
-        thread.join();
-        System.out.println(thread.getState());
-        System.out.println("Method main ends");
+    public static void main(String[] args) {
+        int a[]  = {1, 2, 053, 4};
+        int b[][] = {{1, 2, 4}, {2, 2, 1}, {0, 43, 2}};
+        System.out.print(a[3] == b[0][2]);
+        System.out.print("" + (a[2] == b[2][1]));
+
     }
-}
-class Worker implements Runnable{
 
-    @Override
-    public void run() {
-        System.out.println("work begins");
-        try {
-            Thread.sleep(2500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    class Worker implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("work begins");
+            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("work ends");
         }
-        System.out.println("work ends");
     }
 }
--- src/work_with_files/RandomAccessFile.java
+++ src/work_with_files/RandomAccessFile.java
@@ -1,4 +1,31 @@
 package work_with_files;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class RandomAccessFile {
+
+    public static void main(String[] args) {
+        try (java.io.RandomAccessFile file = new java.io.RandomAccessFile("test10.txt", "rw")) {
+
+            int a = file.read();
+            System.out.println((char) a);
+            file.seek(30);
+            String s1 = file.readLine();
+            System.out.println(s1);
+
+            long position = file.getFilePointer();
+            System.out.println(position);
+
+//            file.seek(0);
+//            file.writeBytes("hi!");
+            file.seek(file.length()-1);
+            file.writeBytes("\n Author");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
